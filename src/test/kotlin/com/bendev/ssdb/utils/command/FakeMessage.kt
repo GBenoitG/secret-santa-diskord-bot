1c0f8d358a9354abb19c9289e8b44a90bac97a3d
Benoit <contact@benoitguillot.fr>
2020-10-20 17:32:46 +0200

    :white_check_mark: | add test for registration content

--- src/test/kotlin/com/bendev/ssdb/utils/command/FakeMessage.kt
+++ src/test/kotlin/com/bendev/ssdb/utils/command/FakeMessage.kt
@@ -5,4 +5,6 @@ object FakeMessage {
     const val FAKE_VALID_RAW_MESSAGE_INVITATION =
         "This is a raw message well formatted :\n✅ I'm in\n❌ No I skip\n⏲ I think about it"
 
+    const val FAKE_VALID_RAW_MESSAGE_START_REGISTRATION = "start"
+
 }
